4b503a7cf32eb67c67ed7f989a21673dc435d0a1
harsweet <hsbhangu0001@gmail.com>
2022-11-19 21:23:13 -0700

    Added config setup for Mongo Atlas connection

---
+++ server/db/config.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const setupDb = (dbUrl) => {
+    const connectionParams = {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true 
+    };
+
+    mongoose.connect(url,connectionParams)
+
+    mongoose.connection.on('connected', () => {
+        console.log('The Mongo Database has connected succesfully')
+    })
+    mongoose.connection.on('reconnected', () => {
+        console.log('The Mongo Database has reconnected')
+    })
+    mongoose.connection.on('error', error => {
+        console.log('The Mongo connection has faced an error', error)
+        mongoose.disconnect()
+    })
+    mongoose.connection.on('disconnected', () => {
+        console.log('The Mongo connection is disconnected')
+    })
+};
+
+export default setupDb;
