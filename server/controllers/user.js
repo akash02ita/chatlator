957fe75a7ac0a42ca7778c6cb5a0b68e2e2dd329
harsweet <hsbhangu0001@gmail.com>
2022-11-19 21:14:14 -0700

    Added schema for user

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -2,7 +2,7 @@
 const usersController = {
     getOneUser: async (req, res) => {
         return res.status(200).json({
-            id: "dfjknfdkjnfdkj23dnjdsnfjsdn",
+            guid: "12d9d84c2cf243f1ae8754d5d8883087",
             name: "ABC",
             username: "userABC",
             email: "abc@gmail.com",
---
+++ server/models/user.js
@@ -0,0 +1,82 @@
+import { randomUUID } from "crypto";
+import mongoose from "mongoose";
+
+export const PRIMARY_LANGUAGES = {
+    ENGLISH: "English",
+    FRENCH: "French",
+};
+
+
+const userSchema = new mongoose.Schema(
+    {
+        guid: {
+            type: String,
+            default: () => randomUUID().replace(/\-/g, ""),
+            unique: true
+        },
+        name: String,
+        username: {
+            type: String,
+            unique: true
+        },
+        email: {
+            type: String,
+            unique: true
+        },
+        primaryLanguage: {
+            type: String,
+            enum: PRIMARY_LANGUAGES,
+        },
+        timestamps: true,
+        collection: "users",
+    }
+);
+
+userSchema.statics.createUser = async function (name, username, email, primaryLanguage) {
+    try {
+        const user = await this.create({ name, username, email, primaryLanguage });
+        return user;
+    } catch (error) {
+        throw error;
+    }
+};
+
+userSchema.statics.getUsers = async function () {
+    try {
+        const users = await this.find();
+        return users;
+    } catch (error) {
+        throw error;
+    }
+};
+
+userSchema.statics.getUserByGuids = async function (givenGuids) {
+    try {
+        const users = await this.find({ guid: { $in: givenGuids } });
+        if (!users) throw ({ error: 'No users found' });
+        return users;
+    } catch (error) {
+        throw error;
+    }
+};
+
+userSchema.statics.getUserByGuid = async function (givenGuid) {
+    try {
+        const user = await this.findOne({ guid: givenGuid });
+        if (!user) throw ({ error: 'No user found' });
+        return user;
+    } catch (error) {
+        throw error;
+    }
+};
+
+userSchema.statics.deleteByUserById = async function (givenGuid) {
+    try {
+        const result = await this.remove({ guid: givenGuid });
+        return result;
+    } catch (error) {
+        throw error;
+    }
+};
+
+export default mongoose.model("User", userSchema);
