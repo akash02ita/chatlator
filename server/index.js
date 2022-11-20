06e77bb905acdcbe3af6b36a298dcb4004721c51
harsweet <hsbhangu0001@gmail.com>
2022-11-19 17:40:20 -0700

    Added dotenv support for secure content

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
+import dotenv from "dotenv";
 import express from "express";
 import http from "http";
 
 import usersRouter from "./routes/user.js";
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
