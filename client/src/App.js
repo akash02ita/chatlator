d2b39b9258c6de12a2d5d25b46f9be91815c3b39
Akashdeep Singh <akash02ita@gmail.com>
2022-12-04 19:29:35 -0700

    frontend: Chat.js sample file

--- client/src/App.js
+++ client/src/App.js
@@ -5,14 +5,15 @@ import React from 'react';
 import Sample1 from './junk_tests_experiments/Sample1';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
-
+import Chat from './junk_tests_experiments/Chat';
 
 function App() {
   return (
     <div className="App">
       {/* <Sample1 />  */}
-      <SignIn />
+      {/* <SignIn /> */}
       {/* <SignUp /> */}
+      <Chat />
     </div>
   );
 }
