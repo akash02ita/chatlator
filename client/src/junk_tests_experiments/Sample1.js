a2e0afe0600e2395aa9c189559a6872a1ef76033
Akashdeep Singh <akash02ita@gmail.com>
2022-12-04 18:18:15 -0700

    frontend: backup test samples. ISSUE: must downgrade to React 17 issue: mui is compatible up to React 17. Not all imports work properly.

---
+++ client/src/junk_tests_experiments/Sample1.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { Button, Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import SearchIcon from '@mui/icons-material/Search';
+import Switch from '@mui/material/Switch'
+import PeopleIcon from '@mui/icons-material/People'; // friends
+import PersonAddIcon from '@mui/icons-material/PersonAdd'; // add friend
+import ChatIcon from '@mui/icons-material/Chat';
+import SettingsIcon from '@mui/icons-material/Settings';
+import PersonAddAltIcon from '@mui/icons-material/PersonAddAlt'; // search new people
+import SendIcon from '@mui/icons-material/Send';
+import CircularProgress from '@mui/material/CircularProgress'
+import LinearProgress from '@mui/material/LinearProgress'
+
+export default function Sample1() {
+    return (
+        <React.Fragment>
+            <CssBaseline />
+            <Button variant="contained" sx={{ color: "yellow" }}>Hello world</Button>
+            <Button color='error' variant="contained">Hello world</Button>
+            <br />
+            <Typography variant="h1">Hello world Typography</Typography>
+            <Typography variant="h4">Hello world Typography</Typography>
+            <Typography variant="h6">Hello world Typography</Typography>
+            <Typography variant="body1">Hello world Typography</Typography>
+            <Typography variant="body2">Hello world Typography</Typography>
+            <br />
+            <AddIcon fontSize='small' />
+            <AddIcon fontSize='medium' />
+            <AddIcon fontSize='large' />
+            <br />
+            <SearchIcon fontSize='small' />
+            <SearchIcon fontSize='medium' />
+            <SearchIcon fontSize='large' onClick={() => console.log("test")} />
+            <br />
+            <Switch aria-label='' />
+            <br />
+            <PeopleIcon fontSize='small' />
+            <PeopleIcon fontSize='medium' />
+            <PeopleIcon fontSize='large' />
+            <br />
+            <ChatIcon fontSize='small' />
+            <ChatIcon fontSize='medium' />
+            <ChatIcon fontSize='large' />
+            <br />
+            <SettingsIcon fontSize='small' />
+            <SettingsIcon fontSize='medium' />
+            <SettingsIcon fontSize='large' />
+            <br />
+            <PersonAddIcon fontSize='small' />
+            <PersonAddIcon fontSize='medium' />
+            <PersonAddIcon fontSize='large' />
+            <br />
+            <PersonAddAltIcon fontSize='small' />
+            <PersonAddAltIcon fontSize='medium' />
+            <PersonAddAltIcon fontSize='large' />
+            <br />
+            <SendIcon fontSize='small' />
+            <SendIcon fontSize='medium' />
+            <SendIcon fontSize='large' />
+            <br />
+            <CircularProgress />
+            <br />
+            <LinearProgress />
+        </React.Fragment>
+    );
+}
