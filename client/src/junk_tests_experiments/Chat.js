90e1434b57aff75bdc032c5881971dd19df85081
Akashdeep Singh <akash02ita@gmail.com>
2022-12-04 20:37:17 -0700

    frontend: Chat sample: fix console log warning

--- client/src/junk_tests_experiments/Chat.js
+++ client/src/junk_tests_experiments/Chat.js
@@ -121,7 +121,7 @@ const Chat = () => {
                         <Grid item xs={11}>
                             <TextField id="outlined-basic-email" label="Type Something" fullWidth />
                         </Grid>
-                        <Grid xs={1} align="right">
+                        <Grid item xs={1} align="right">
                             <Fab color="primary" aria-label="add"><SendIcon /></Fab>
                         </Grid>
                     </Grid>
