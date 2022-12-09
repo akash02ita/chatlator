57c4336fe0f872142de1441c95a7212c0567cabe
Akashdeep Singh <akash02ita@gmail.com>
2022-12-08 18:11:30 -0700

    frontend: sign up and sign in linking set up forgot password not yet linked anywhere

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -1,8 +1,15 @@
-
+import { Link } from "react-router-dom";
+import SignIn from "./SignIn";
 export default function Welcome() {
     return (
-        <div>
-            welcome
-        </div>
+        // <div>
+        //     <h2>
+        //         <Link to="SignUp">Click to view sign up</Link>
+        //     </h2>
+        //     <h2>
+        //         <Link to="SignIn">Click to view sign in</Link>
+        //     </h2>
+        // </div>
+        <SignIn />
     );
 }
