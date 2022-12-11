34c872971d871a263c898b2adf843937753f77a5
CarlosVeinti <carlosveintimillam@hotmail.com>
2022-12-11 15:34:19 -0700

    Idea for Trying to pair up Random People
    An idea for handling the pairing of two random people

--- server/controllers/random.js
+++ server/controllers/random.js
@@ -1,5 +1,15 @@
 const liveData = {};
 
+const whoIsLookingForRP = {}; // Who is looking for random people
+
+const whoIsTryingTo_PU = {}; // Who is trying to pair up.
+// Might need to be some sort of dictionary? 
+    // In essence, each user, is gonna have another structure, could be a list, array
+    // but all those elements in there would indicate that these "elements/users" have an interest 
+    // in having a chat with the user of the "key"
+        // so maybe the data structure could be a Dictionary as is
+            // inside of it, however, there are key -> Array 
+
 const updateLiveData = ({ username, email, primaryLanguage, learnLanguage }) => {
     const key = username;
     liveData[primaryLanguage][key] = learnLanguage;
@@ -20,9 +30,10 @@ const findMatches = ({ username, email, primaryLanguage, learnLanguage }) => {
 
     return [];
 }
-
+// This part here, is the only one building a response. Two const above support this.
 const randomController = {
-    handleRandomUsersSearch: async (req, res) => {
+    handleRandomUsersSearch: async (req, res) => { // why async? does it need to be?
+        // When we get a user, it does not come with learnLanguage in the "user" object.
         const { username, email, primaryLanguage, learnLanguage } = req.body;
 
         if (!(primaryLanguage in liveData)) {
@@ -37,5 +48,15 @@ const randomController = {
         return res.status(200).json({ "matches": matches });
     }
 };
+
+
+
+// const randomController = { handleRandomUsers_PR };
+
+// basically what the function would look like under the "model" directory.
+randomController.statics.handleRandomUsers_PR = async(req, res) => { // Handle Random Users Pair Request.
+    const { username, email, primaryLanguage, learnLanguage } = req.body;
+    
+}
 
 export default randomController;
--- server/routes/random.js
+++ server/routes/random.js
@@ -4,6 +4,7 @@ import express from "express";
 const randomRouter = express.Router();
 
 randomRouter
-    .post('/', randomController.handleRandomUsersSearch);
+    .post('/', randomController.handleRandomUsersSearch)
+    .post('/', randomController.handleRandomUsers_PR); // PR = Pair Request.
 
 export default randomRouter;
