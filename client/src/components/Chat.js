c60c58ae8e582f5580338734e24eeb8a80650984
Akashdeep Singh <akash02ita@gmail.com>
2022-12-12 07:12:23 -0700

    quick fix on merge

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -27,26 +27,6 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import Slide from '@material-ui/core/Slide';
 import AddCircleOutlineRoundedIcon from '@material-ui/icons/AddCircleOutlineRounded';
 
-const useStyles = makeStyles({
-    table: {
-        minWidth: 650,
-    },
-    chatSection: {
-        width: '100%',
-        height: '80vh'
-    },
-    headBG: {
-        backgroundColor: '#e0e0e0'
-    },
-    borderRight500: {
-        borderRight: '1px solid #e0e0e0'
-    },
-    messageArea: {
-        height: '70vh',
-        overflowY: 'auto'
-    }
-});
-
 // for dialog
 const Transition = React.forwardRef(function Transition(props, ref) {
     return <Slide direction="up" ref={ref} {...props} />;
