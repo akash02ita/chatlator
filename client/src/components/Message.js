61046df601b19dba61c5987de25ac712f35e2240
nicholas-mclaughlin <nickryu4718@gmail.com>
2022-12-12 00:41:29 -0700

    Styling for messages

--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -1,15 +1,23 @@
+import Card from '@mui/material/Card';
 import Grid from '@material-ui/core/Grid';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 
 export default function Message(props) {
-  // console.log(props)
+  let originalTextBg = '#19A3FE'
+  let translatedTextBg = '#100252'
+  let textColor = 'white'
+  if (props.side == "left") {
+    originalTextBg = '#a4f97c'
+    translatedTextBg = '#53d769'
+    textColor = 'black'
+  }
   return (
     <ListItem>
       <Grid container>
         <Grid item xs={12}>
-          <ListItemText align={props.side} primary={props.contentOriginal}></ListItemText>
-          <ListItemText align={props.side} primary={props.contentTranslated}></ListItemText>
+          <ListItemText align={props.side} primary={<Card className='message' sx={{ bgcolor: originalTextBg, color: textColor }}>{props.contentOriginal}</Card>}></ListItemText>
+          <ListItemText align={props.side} primary={<Card className='message' sx={{ bgcolor: translatedTextBg, color: textColor }}>{props.contentTranslated}</Card>}></ListItemText>
         </Grid>
         <Grid item xs={12}>
           <ListItemText align={props.side} secondary={props.updatedAt}></ListItemText>
