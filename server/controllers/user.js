05f7c921a9e2a0255eb80e098100bc4953c69220
Akashdeep Singh <akash02ita@gmail.com>
2022-12-14 00:09:43 -0700

    no longer creating rooms at sign up

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -5,23 +5,23 @@ const usersController = {
     handleCreateUser: async (req, res) => {
         try {
             const { name, email, password, primaryLanguage } = req.body;
-            const previousUsers = await UserModel.find({});
+            // const previousUsers = await UserModel.find({});
             const newUser = await UserModel.createUser(name, email, password, primaryLanguage);
-            const userInfos = [];
-            previousUsers.forEach((puser) => {
-                const userInfo = {};
-                userInfo[puser.guid] = {"primaryLanguage": puser.primaryLanguage, "name": puser.name};
-                userInfo[newUser.guid] = {"primaryLanguage": newUser.primaryLanguage, "name": newUser.name};
-                // console.log("new user info is ", userInfo);
-                userInfos.push(userInfo);
-            });
-            for (const userInfo of userInfos) {
-                try {
-                    const newRoom = await ChatRoomModel.createChatRoom(userInfo);
-                } catch (error) {
-                    return res.status(500).json({ success: false, message:"Error creating the accounts", error: error });
-                }
-            }
+            // const userInfos = [];
+            // previousUsers.forEach((puser) => {
+            //     const userInfo = {};
+            //     userInfo[puser.guid] = {"primaryLanguage": puser.primaryLanguage, "name": puser.name};
+            //     userInfo[newUser.guid] = {"primaryLanguage": newUser.primaryLanguage, "name": newUser.name};
+            //     // console.log("new user info is ", userInfo);
+            //     userInfos.push(userInfo);
+            // });
+            // for (const userInfo of userInfos) {
+            //     try {
+            //         const newRoom = await ChatRoomModel.createChatRoom(userInfo);
+            //     } catch (error) {
+            //         return res.status(500).json({ success: false, message:"Error creating the accounts", error: error });
+            //     }
+            // }
             return res.status(200).json({ success: true, message:"Account successfully created", user: newUser });
         } catch(error) {
             return res.status(500).json({ success: false, message:"Error creating the account", error: error });
